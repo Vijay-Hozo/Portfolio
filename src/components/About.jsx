bff8757f14907bb37de39f7ccb57804ce7ba4776
Vijay-Hozo <vijay2304a@gmail.com>
2023-10-31 15:07:42 +0530

    textsize

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,13 +14,13 @@ const About = () => {
               className='text-4xl font-bold inline border-b-4 border-gray-500 font-FD'>ABOUT</p>
             </div>
 
-        <p className='text-xl mt-10 font-semibold text-white'>
+        <p className='text-xl mt-10 font-semibold text-white max-sm:text-sm'>
         Hello, I'm delighted to share a bit about who I am. I'm a dynamic individual with a diverse range of interests and experiences that have shaped my identity.
         I.m currently pursuing B.TECH IT in Coimbatore.
         In my professional life, I've had the privilege of Frontend Developer, which I continue to develop and apply in various aspects of my life.
         </p>
         <br />
-        <p className='text-xl font-semibold  text-white'>
+        <p className='text-xl font-semibold  text-white max-sm:text-sm'>
         I'm a problem-solver at heart, adept at troubleshooting and debugging to deliver optimal solutions under tight deadlines. I stay up-to-date with the latest industry trends and emerging technologies, ensuring that the projects I work on are at the forefront of innovation.I'm excited about the ever-evolving landscape of frontend development, and I'm committed to creating exceptional user experiences through clean, maintainable, and performant code. Let's work together to transform web ideas into reality.
         </p>
     </div>
