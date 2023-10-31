dccc9f4f41c82dd89018fed55a5d587ef5068cdc
Vijay-Hozo <vijay2304a@gmail.com>
2023-10-31 09:16:42 +0530

    icon change

--- src/components/Projectcard.jsx
+++ src/components/Projectcard.jsx
@@ -4,7 +4,7 @@ const Projectcard = (props) => {
       const {id,src,href,code,heading} = props ;
 
   return (
-    <div key={id} className='flex flex-col justify-between shadow-md shadow-gray-600 rounded-lg w-[380px] h-[310px] overflow-hidden'>
+    <div key={id} className='flex flex-col justify-between shadow-md shadow-gray-600 rounded-lg w-[380px] h-[310px] overflow-hidden '>
         <img src={src} alt=""  className='flex-1 rounded-md duration-200 hover:scale-105 '/>
         <p className='flex justify-center pt-3 font-semibold'>{heading}</p>
             <div className='flex-2 flex items-center justify-center'>
